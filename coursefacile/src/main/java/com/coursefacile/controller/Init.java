c0d14f2e86de3043bc95c75476ca5a03bb15b947
abdelMad <abdelmoghit.madih@gmail.com>
2018-04-02 16:17:49 +0200

    Correction de l'erreur de Init servlet

--- coursefacile/src/main/java/com/coursefacile/controller/Init.java
+++ coursefacile/src/main/java/com/coursefacile/controller/Init.java
@@ -1,6 +1,7 @@
 package com.coursefacile.controller;
 
 import com.coursefacile.dao.SessionFactoryHelper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,15 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class Init extends HttpServlet {
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         SessionFactoryHelper.init();
-        System.out.println(this.getServletContext().getInitParameter("prefixPath"));
         this.getServletContext().getRequestDispatcher("/views/index.jsp").forward(request, response);
-
     }
 }
